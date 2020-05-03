2ce949fc1e9e5087150f03f61a2133d66e03c2ff
celalaksu <32665644+celalaksu@users.noreply.github.com>
2020-05-03 23:20:09 +0300

    Proje dosyası ekle.

---
+++ VeriBaglamaMVVM/VeriBaglamaMVVM/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace VeriBaglamaMVVM
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string isim = string.Empty;
+        public string Isim
+        {
+            get => isim;
+            set
+            {
+                if (isim == value)
+                {
+                    return;
+                }
+
+                isim = value;
+                OnPropertyChanged(nameof(isim));
+                OnPropertyChanged(nameof(IsmiGoster));
+            }
+        }
+
+        public string IsmiGoster => $"Name Entered: {Isim}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
